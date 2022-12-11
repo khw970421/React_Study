aa4e115f874dc1c2f936a2b23073de23ef24c49b
khw970421 <khw970421@kakao.com>
2022-12-11 14:24:07 +0900

    ♻️ refacor : random 접근에서 e.target으로 수정

--- BtnCount/VanillaJS/Components/Counter/index.js
+++ BtnCount/VanillaJS/Components/Counter/index.js
@@ -1,33 +1,28 @@
-export default function Counter({ $target , onIncrease , onDecrease }){
-  const $div = document.createElement('div');
-  $target.appendChild($div)
-  let count = 0
+export default function Counter({ $target, onIncrease, onDecrease }) {
+  const $div = document.createElement("div");
+  $target.appendChild($div);
+  let count = 0;
 
-  this.render = ()=>{
-    const random = Math.floor(Math.random() * 1000000)
-    const increaseId = `increase${random}`
-    const decreaseId = `decrease${random}`
-
+  this.render = () => {
     $div.innerHTML = `
       <span id="count" style="font-size:40px">0</span><br/>
-      <button id=${increaseId}>+</button>
-      <button id=${decreaseId}>-</button>
-     `
+      <button id="plus">+</button>
+      <button id="minus">-</button>
+     `;
 
-    document.querySelector(`#${increaseId}`).addEventListener('click',()=>{
-      count++;
-      $div.querySelector('#count').innerHTML = String(count)
-      onIncrease($target , count);
-    })
-
-    document.querySelector(`#${decreaseId}`).addEventListener('click',()=>{
-      count--;
-      $div.querySelector('#count').innerHTML = String(count)
-      onDecrease($target ,count);
-    })
-  }
+    $div.addEventListener("click", (e) => {
+      if (e.target.nodeName === "BUTTON") {
+        if (e.target.id === "plus") {
+          count++;
+          onIncrease($target, count);
+        } else if (e.target.id === "minus") {
+          count--;
+          onDecrease($target, count);
+        }
+        $div.querySelector("#count").innerHTML = String(count);
+      }
+    });
+  };
 
   this.render();
 }
-
-
